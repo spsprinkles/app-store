b8fa625840ecfbe24a22f9742e476a555849557a
Gunjan Datta <gudatta@microsoft.com>
2023-05-31 07:27:35 -0400

    Implemented the custom file input.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import { Dashboard } from "dattatable";
+import { Dashboard, LoadingDialog } from "dattatable";
+import { Components, Helper } from "gd-sprest-bs";
 import * as jQuery from "jquery";
 import { DataSource, IAppStoreItem } from "./ds";
 import Strings from "./strings";
-import { Components, SPTypes } from "gd-sprest-bs";
 
 /**
  * Main Application
@@ -20,8 +20,58 @@ export class App {
         props.onControlRendering = (ctrl, fld) => {
             // See if this is a url field
             if (fld.InternalName == "Icon" || fld.InternalName.indexOf("ScreenShot") == 0) {
-                // TODO
-                // Create a custom component for converting images to base64
+                // Make the field read-only
+                ctrl.isReadonly = true;
+            }
+            // Else, see if this is a link
+            else if (fld.InternalName == "AdditionalInformation") {
+                // Update the props
+                (ctrl as Components.IFormControlUrlProps).showDescription = false;
+            }
+        }
+
+        // Set the control rendered event
+        props.onControlRendered = (ctrl, fld) => {
+            // See if this is a url field
+            if (fld.InternalName == "Icon" || fld.InternalName.indexOf("ScreenShot") == 0) {
+                // Set a click event
+                ctrl.textbox.elTextbox.addEventListener("click", () => {
+                    // Display a file upload dialog
+                    Helper.ListForm.showFileDialog().then(file => {
+                        // Clear the value
+                        ctrl.textbox.setValue("");
+
+                        // Get the file name
+                        let fileName = file.name.toLowerCase();
+
+                        // Validate the file type
+                        if (fileName.endsWith(".png") || fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".gif")) {
+                            // Show a loading dialog
+                            LoadingDialog.setHeader("Reading the File");
+                            LoadingDialog.setBody("This will close after the file is converted...");
+                            LoadingDialog.show();
+
+                            // Convert the file
+                            let reader = new FileReader();
+                            reader.onloadend = () => {
+                                let value = reader.result as string;
+
+                                // Set the value
+                                ctrl.textbox.setValue(value.replace("data:", '').replace(/^.+,/, ''));
+
+                                // Close the dialog
+                                LoadingDialog.hide();
+                            }
+                            reader.readAsDataURL(file.src);
+                        } else {
+                            // Display an error message
+                            ctrl.updateValidation(ctrl.el, {
+                                isValid: false,
+                                invalidMessage: "The file must be a valid image file. Valid types: png, jpg, jpeg, gif"
+                            });
+                        }
+                    });
+                });
             }
         }
 
